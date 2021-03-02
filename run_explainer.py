(Fire/WIP) Added config. Refactoring from gxai. input_ids have wrong shape.

run_explainer.py
@@ -5,11 +5,11 @@ import os
 import torch
 
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 import thermometer.explainers as thermex
-from thermometer.data.dtypes import DatapointColored, DatapointProcessed
-from thermometer.data.readers import DatasetProcessedDatapoints, JsonIterator, ShapleyJsonToTensor
-from thermometer.utils import get_logger, get_time, read_config, read_path
+from thermometer.data.readers import get_dataset
+from thermometer.utils import detach_to_list, get_logger, get_time, read_config, read_path
 
 
 config = read_config('configs/exp-a01_imdb_LayerIntegratedGradients_textattack-roberta-base-imdb.jsonnet')
@@ -28,63 +28,59 @@ experiment_in = [f for f in os.listdir(experiment_path)
                  if "preprocess" in f and explainer_name not in f and f.endswith('.jsonl')][0]
 path_in = os.path.join(experiment_path, experiment_in)
 logger.info(f'(File I/O) Input file: {path_in}')
+path_out = f'{read_path(experiment_path)}/{_now}.{explainer_name}.{experiment_in}'
+logger.info(f'(File I/O) Output file: {path_out}')
+assert not os.path.isfile(path_out), f'File {path_out} already exists!'
 
+# Log config
 logger.info(f'(Config) Config: \n{json.dumps(config, indent=2)}')
 
+# Device
 torch.cuda.empty_cache()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 logger.info(f'(Config) Explaining on device: {device}')
 
-path_out = f'{read_path(experiment_path)}/{_now}.{explainer_name}.{experiment_in}'
-logger.info(f'(File I/O) Output file: {path_out}')
+# Dataset
+dataset_config = config['dataset']
+dataset_config['tokenizer'] = config['model']['tokenizer']
+dataset = get_dataset(config=dataset_config)
+config['dataset']['num_labels'] = len(dataset.features['label'].names)
 
+# Explainer
 explainer = getattr(thermex, f'Explainer{explainer_name}').from_config(config=config)
 explainer.to(device)
-
-logger.info(f'(Progress) Loaded trainer')
-
-dataset_config = read_config(config['dataset']['config'])
-dataset_config['path_in'] = path_in
-preprocess_config = read_config(config['model']['tokenizer'])
-dataset_config['name_input'] = preprocess_config['name_tokenizer']
-dataset = DatasetProcessedDatapoints.from_config(config=dataset_config)
-
 batch_size = config['explainer']['internal_batch_size']
+logger.info(f'(Progress) Loaded explainer')
+
+# DataLoader
 dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=False)
-
-json_iterator = JsonIterator()
-
 logger.info(f'(Progress) Initialized data loader')
 
 file_out = open(path_out, 'w+')
-for idx, (batch_tensors, batch_lines) in enumerate(zip(dataloader,
-                                                       json_iterator(path_in=path_in,
-                                                                     batch_size=batch_size))):
-    if 0 < config['explainer']['early_stopping'] < idx:
-        logger.info(f'Stopping early after {idx} batches.')
-        break
-    attribution, predictions = explainer.explain(batch_tensors)
-    attribution = attribution.detach().cpu().numpy().tolist()
-    if predictions is not None:  # generative explaines do not return predictions of the downstream model
-        predictions = predictions.detach().cpu().numpy().tolist()
-    for i, line in enumerate(batch_lines):
-        json_line = json.loads(line)
-        if isinstance(dataset, ShapleyJsonToTensor):  # legacy code starts indexing at zero
-            assert (json_id := json_line['id']) == (batch_id := (batch_tensors['id'][i].item() + 1)), \
-                f'Sanity check failed: Ids should match, but {json_id} != {batch_id}'
-        else:
-            assert json_line['id'] == batch_tensors['id'][i].item(), 'Sanity check failed: Ids should match'
-        explanation = {'attribution': attribution[i],
-                       'prediction': predictions[i] if predictions is not None else None,
-                       'config': config}
 
-        p_datapoint = DatapointProcessed.from_dict(json_line)
-        c_datapoint = DatapointColored.from_parent_class(datapoint_processed=p_datapoint,
-                                                         name_explanation=explainer_name,
-                                                         explanation=explanation)
+for idx_batch, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+    logger.info(f'(Progress) Processing batch {idx_batch} / instance {idx_batch * batch_size}')
+    attribution, predictions = explainer.explain(batch)
 
-        file_out.write(str(c_datapoint) + os.linesep)
-        logger.info(f'(Progress) Gave {(idx + 1) * batch_size} explanations')
+    for idx_instance in range(len(batch['input_ids'])):
+        idx_instance_running = (idx_batch * batch_size)
+
+        ids = detach_to_list(batch['input_ids'][idx_instance])
+        labels = detach_to_list(batch['labels'][idx_instance])
+        attrbs = detach_to_list(attribution[idx_instance])
+        preds = detach_to_list(predictions[idx_instance])
+        result = {'dataset': dataset_config,
+                  'batch': idx_batch,
+                  'instance': idx_instance,
+                  'index_running': idx_instance_running,
+                  'explainer': config['explainer'],
+                  'input_ids': ids,
+                  'labels': labels,
+                  'attributions': attrbs,
+                  'predictions': preds,
+                  'path_model': config['model']['path_model']}
+
+        file_out.write(json.dumps(result) + os.linesep)
 
 logger.info('(Progress) Terminated normally.')
 logger.info(f'(Progress) Done training {str(explainer)} explainer; wrote model weights to {path_out}')

src/thermometer/data/readers.py
@@ -1,166 +1,53 @@
-import json
-import torch
-from overrides import overrides
-from torch.utils.data import Dataset
-from typing import Dict, List
+from datasets import load_dataset, load_from_disk
+from transformers import AutoTokenizer
+from typing import Dict
 
-from thermometer.utils import read_path, Configurable
+from thermometer.utils import read_path
 
 
-class DatasetProcessedDatapoints(Configurable, Dataset):
-
-    def __init__(self):
-        super().__init__()
-        self.name = None
-        self.path_in = None
-        self.size = None
-        self.name_input = None
-        self.input_keys = None
-        self.splits: List = []
-        self.num_labels = None
-
-    def __len__(self):
-        return self.size
-
-    @staticmethod
-    def no_of_lines(path_in):
-        fin = open(path_in, 'r+')
-        result = len(fin.readlines())
-        fin.close()
-        return result
-
-    @overrides
-    def __getitem__(self, idx):
-        file_in = open(self.path_in, 'r+')
-        for idx_line, line in enumerate(file_in.readlines()):
-            if idx_line == idx:
-                json_line = json.loads(line)
-                tensors = json_line['inputs'][self.name_input]['tensors']
-                tensors['id'] = torch.LongTensor([json_line['id']])
-                tensors = {k: torch.LongTensor(v) for k, v in tensors.items() if k in self.input_keys}
-                return tensors
-
-    @overrides
-    def validate_config(self, config: Dict) -> bool:
-        assert 'input_keys' in config, 'Specify input keys'
-
-    @classmethod
-    def from_config(cls, config: Dict):
-        res: DatasetProcessedDatapoints = super().from_config(config)
-        res.size = res.no_of_lines(res.path_in)
-        return res
+def download_dataset(config: Dict, logger):
+    """
+    :param config: "dataset" sub-config of a jsonnet config
+    """
+    assert all(x in config for x in ['name', 'split', 'root_dir'])
+    dataset = load_dataset(config['name'], split=config['split'])
+    root_dir = read_path(config['root_dir'])
+    dataset.save_to_disk(dataset_path=f'{root_dir}/{config["name"]}')
+    logger.info(f'(Progress) Terminated normally')
 
 
-class DatasetColoredDatapoints(Configurable, Dataset):
-    """A dataset reader which gives additional access to attributions, i.e. to train a generative explainer."""
+def get_dataset(config: Dict):
+    """ Returns a pytorch dataset from a config. """
+    assert 'tokenizer' in config
+    tokenizer_config = config['tokenizer']
 
-    def __init__(self):
-        super().__init__()
-        self.path_in = None  # path_in
-        self.size = None  # self.no_of_lines(self.path_in)
-        self.name_input = None  # name_input
-        self.name_explanation = None
-        self.name_model = None  # name_model
-        self.mode = None  # mode
-        # self.keys = None # self.keys_options[f'{self.name_model}.{self.mode}']
-        self.input_keys = None
-        self.flip_attributions_on_index = None
+    def encode(instances):
+        return tokenizer(instances[text_field],
+                         truncation=tokenizer_config['truncation'],
+                         padding=tokenizer_config['padding'],
+                         max_length=tokenizer_config['max_length'],
+                         return_special_tokens_mask=tokenizer_config['special_tokens_mask'])
 
-    def __len__(self):
-        return self.size
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_config['name'])
+    dataset = load_from_disk(f'{read_path(config["root_dir"])}/{config["name"]}')
 
-    @staticmethod
-    def no_of_lines(path_in):
-        fin = open(path_in, 'r+')
-        result = len(fin.readlines())
-        fin.close()
-        return result
+    dataset = dataset.select(indices=get_dataset_index_range(dataset, config))
 
-    @overrides
-    def __getitem__(self, idx):
-        file_in = open(self.path_in, 'r+')  # todo: very inefficient, consider keeping file reader open
-        for idx_line, line in enumerate(file_in.readlines()):
-            if idx_line == idx:
-                json_line = json.loads(line)
-                tensors = json_line['inputs'][self.name_input]['tensors']
-                tensors['id'] = torch.LongTensor([json_line['id']])
-                tensors = {k: torch.LongTensor(v) for k, v in tensors.items() if k in self.input_keys}
-                attributions = json_line['explanation'][self.name_explanation]['attribution']
-                if self.flip_attributions_on_index is not None:
-                    assert isinstance(self.flip_attributions_on_index, int), "flip_attributions_on_index should be int"
-                    prediction = json_line['explanation'][self.name_explanation]['prediction']
-                    max_idx = prediction.index(max(prediction))
-                    if max_idx == self.flip_attributions_on_index:
-                        attributions = [score * -1 for score in attributions]
-                tensors['attribution'] = torch.Tensor(attributions)
-                return tensors
-
-    @overrides
-    def validate_config(self, config: Dict) -> bool:
-        assert 'path_in' in config, 'Specify input path'
-        assert 'name_input' in config, 'Specify input name'
-        assert 'input_keys' in config, 'Specify input keys'
-        return True
-
-    @classmethod
-    def from_config(cls, config: Dict):
-        res: DatasetProcessedDatapoints = super().from_config(config)
-        res.path_in = read_path(res.path_in)
-        res.size = res.no_of_lines(res.path_in)
-        assert "labels" not in res.input_keys, "Sanity check failed."
-        return res
+    text_field = 'text' if 'text_field' not in config else config['text_field']
+    dataset = dataset.map(encode, batched=True, batch_size=config['batch_size'])
+    dataset = dataset.map(lambda examples: {'labels': examples['label']},
+                          batched=True, batch_size=config['batch_size'])
+    dataset.set_format(type='torch', columns=config['columns'])
+    return dataset
 
 
-class JsonIterator:
+def get_dataset_index_range(dataset, dataset_config):
+    start = 0 if 'start' not in dataset_config else dataset_config['start']
+    if start < 0:
+        start = 0
 
-    def __call__(self, path_in: str, batch_size: int):
-        self.path_in = path_in,
-        self.batch_size = batch_size
-        self.file_in = open(path_in, 'r+')
-        return self
+    end = len(dataset) if 'end' not in dataset_config else dataset_config['end']
+    if end < 0:
+        end = len(dataset)
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        batch = []
-        for _ in range(self.batch_size):
-            batch.append(self.file_in.readline())
-        return batch
-
-
-class ShapleyJsonToTensor(Configurable, Dataset):
-
-    def __init__(self):
-        self.path_in = None
-        self.size = None
-
-    def validate_config(self, config: Dict) -> bool:
-        assert 'path_in_shapley' in config, 'No input path found.'
-        assert 'name_model' in config, 'No model name found.'
-        return True
-
-    @classmethod
-    def from_config(cls, config: Dict):
-        res = cls()
-        res.path_in = read_path(config['path_in_shapley'])
-        res.name_model = config['name_model']
-        res.size = DatasetColoredDatapoints.no_of_lines(res.path_in)
-        return res
-
-    def __len__(self):
-        return self.size
-
-    def __getitem__(self, idx):
-        file_in = open(self.path_in, 'r+')  # todo: very inefficient, consider keeping file reader open
-        for idx_line, line in enumerate(file_in.readlines()):
-            if idx_line == idx:
-                json_line = json.loads(line)
-                assert (json_name := json_line['model_name']) == self.name_model,\
-                    f'Model names differ {json_name} != {self.name_model} sanity check failed.'
-                tensors = {'id': [json_line['id']],
-                           'n_samples': [json_line['n_samples']],
-                           'attribution': json_line['scores'],
-                           'prediction': json_line['prediction']}
-                tensors = {k: torch.Tensor(v) for k, v in tensors.items()}
-                return tensors
+    return range(start, end)