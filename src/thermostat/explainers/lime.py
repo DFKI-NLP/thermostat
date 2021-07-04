b4ca85190040a1d6e401aef0b2665a9a65384f45
nfelnlp <feldhusnlp@gmail.com>
2021-07-04 19:53:00 +0200

    Fixed interpretable model in Lime.

--- src/thermostat/explainers/lime.py
+++ src/thermostat/explainers/lime.py
@@ -1,6 +1,6 @@
 import torch
 from captum.attr import KernelShap, Lime, LimeBase
-from captum._utils.models.linear_model import SkLearnLinearModel
+from captum._utils.models.linear_model import SkLearnLasso, SkLearnLinearModel
 from functools import partial
 from math import exp
 from typing import Any, Dict, List
@@ -38,7 +38,7 @@ class ExplainerLime(ExplainerAutoModelInitializer):
     def from_config(cls, config):
         res = super().from_config(config)
         res.n_samples = config['explainer']['n_samples']
-        res.explainer = Lime(forward_func=res.forward_func)
+        res.explainer = Lime(forward_func=res.forward_func, interpretable_model=SkLearnLasso(alpha=0.0))
         return res
 
     def explain(self, batch):
@@ -153,26 +153,37 @@ class ExplainerLimeBase(ExplainerAutoModelInitializer):
         return attributions, predictions
 
 
-class ExplainerKernelShap(ExplainerLimeBase):
+class ExplainerKernelShap(ExplainerAutoModelInitializer):
     def __init__(self):
+        raise NotImplementedError
         super().__init__()
         self.internal_batch_size = None
         self.n_samples = None
 
     def validate_config(self, config: Dict) -> bool:
         super().validate_config(config)
-        assert 'internal_batch_size' in config['explainer'], 'Define an internal batch size for the attribute method.'
         assert 'n_samples' in config['explainer'], 'Set number of samples for attribution function.'
-        assert 'mask_prob' in config['explainer'], 'Set probability of masking a token in perturbation function.'
-        assert 0 <= config['explainer']['mask_prob'] <= 1, 'Mask probability must be between 0 and 1.'
 
     @classmethod
     def from_config(cls, config):
         res = super().from_config(config)
         res.validate_config(config)
-        res.internal_batch_size = config['explainer']['internal_batch_size']
         res.n_samples = config['explainer']['n_samples']
-        res.mask_prob = config['explainer']['mask_prob']
-
-        res.explainer = KernelShap(forward_func=res.forward_func)
+        res.explainer = KernelShap(forward_func=res.forward_func, interpretable_model=SkLearnLasso(alpha=0.0))
         return res
+
+    def explain(self, batch):
+        assert len(batch['input_ids']) == 1, 'This implementation assumes that the batch size is 1.'
+        batch = {k: v.to(self.device) for k, v in batch.items()}
+        inputs, additional_forward_args = self.get_inputs_and_additional_args(base_model=type(self.model.base_model),
+                                                                              batch=batch)
+
+        predictions = self.forward_func(inputs, *additional_forward_args)
+        target = torch.argmax(predictions, dim=1)
+        base_line = self.get_baseline(batch=batch)
+        attributions = self.explainer.attribute(inputs=inputs,
+                                                n_samples=self.n_samples,
+                                                additional_forward_args=additional_forward_args,
+                                                target=target,
+                                                baseline=base_line)
+        return attributions, predictions
